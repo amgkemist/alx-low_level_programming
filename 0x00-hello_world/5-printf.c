c50edf1d9f08f01835187f972c6c0e45767f321b
amgkemist <kemistxpress@gmail.com>
2023-06-15 17:56:16 -0700

    5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints a string
  *
  * Return: Always 0(Success)
  */
 int main(void)
 {
-		printf("with proper grammer, but the outcome is a piece of art,");
-		return (0);
+	printf("\"with proper grammer, but the outcome is a piece of art,");
+	return (0);
 }
