f30f273e1f9e857722e141baf97e46db7ad4b9a0
amgkemist <kemistxpress@gmail.com>
2023-06-15 19:19:13 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <std.h>
+#include <unistd.h>
 /**
  * main - A prints quote to the standard error
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", (stderr));
 	return (1);
 }
