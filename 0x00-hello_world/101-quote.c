f35558e14059ec59ed02a7ee6edf65c38b6a8d09
amgkemist <kemistxpress@gmail.com>
2023-06-15 18:37:19 -0700

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints a quote
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", stdout);
+return (1);
