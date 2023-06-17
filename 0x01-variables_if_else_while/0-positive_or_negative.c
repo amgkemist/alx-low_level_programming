a46d9881d9a4b8a7ea3bd10d7bce867894988ad5
amgkemist <kemistxpress@gmail.com>
2023-06-17 03:17:52 -0700

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Determine whether number 
+ * stored in n is positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	if n > 0
+		printf("%d is positive\n", n);
+
+	else if n == 0
+		pritnf("%d is zero\n");
+
+	else
+		printf("%d is negative\n");
+
+	return (0);
+}
